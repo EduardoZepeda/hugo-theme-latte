3e0b9f9f25e01c195629890bdee3b1385c50ed9a
Eduardo Zepeda <eduardozepeda@coffeebytes.dev>
2022-04-19 14:25:38 -0500

    Add SEO metadata, scroll up button and codewars icon

--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,6 @@
 function addSwitchThemeListener(event) {
-    document.documentElement.setAttribute("data-theme", "dark");
-    let themeSwitcher = document.getElementById("toggle-label");
+    document.documentElement.setAttribute("data-theme", "dark")
+    let themeSwitcher = document.getElementById("toggle-label")
     themeSwitcher.onclick = function () {
         let currentTheme = document.documentElement.getAttribute("data-theme");
         let switchToTheme = currentTheme === "dark" ? "light" : "dark"
@@ -9,25 +9,57 @@ function addSwitchThemeListener(event) {
 }
 
 function addSideBarClickListener(event) {
+    const sidebar = document.getElementById("sidebar")
+    const burger = document.getElementById("burger-icon")
+    const close = document.getElementById("close-icon")
     function toggleSideBar() {
         if (sidebar.classList.contains("sidebarHidden")) {
             sidebar.classList.remove("sidebarHidden")
+            burger.setAttribute("aria-expanded", true)
         } else {
             sidebar.classList.add("sidebarHidden")
-
+            burger.setAttribute("aria-expanded", false)
         }
     }
-    const sidebar = document.getElementById("sidebar");
-    const burger = document.getElementById("burger-icon")
-    const close = document.getElementById("close-icon")
-    close.onclick = toggleSideBar;
-    burger.onclick = toggleSideBar;
+    close.onclick = toggleSideBar
+    burger.onclick = toggleSideBar
 
+}
+
+function addScrolltoTopListener(event) {
+    const scrollUp = document.getElementById("scroll-up")
+    let eventTimeout
+    window?.addEventListener('scroll', setCurrentPosition)
+
+    function setCurrentPosition() {
+        if (!eventTimeout) {
+            eventTimeout = setTimeout(() => {
+                eventTimeout = null
+                if (window?.scrollY < 400) {
+                    scrollUp.classList.add("hidden")
+                } else {
+                    scrollUp.classList.remove("hidden")
+                }
+
+            }, 400)
+        }
+    }
+
+    function scrollToTop() {
+        window?.scroll({
+            top: 0,
+            left: 0,
+            behavior: 'smooth'
+        })
+    }
+
+    scrollUp.onclick = scrollToTop
 }
 
 function loadAllListeners(event) {
     addSwitchThemeListener(event)
     addSideBarClickListener(event)
+    addScrolltoTopListener(event)
 }
 
 
