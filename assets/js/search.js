443d5dca49ca2e9472b4abb4db804b49b1d61ca9
Eduardo Zepeda <eduardozepeda@coffeebytes.dev>
2022-04-22 15:44:46 -0500

    Add search functionality based on lunr
    The search functionality works in the following way:
    
    - Hugo generates a JSON index that consist in a JSON object with three
      fields: url, content and title
    - The data is then processed with lunr to generate a similarity index
      with each search
    - The data is processed using vanilla JS

---
+++ assets/js/search.js
@@ -0,0 +1,123 @@
+function initializeLunrSearch(event) {
+    let searchInput = document.getElementById('search-input')
+    let searchResults = document.getElementById('search-results')
+    let documents = []
+    let idx
+    if(!searchInput || !searchResults){
+        return
+    }
+
+    // request previously generated index
+    fetch('/posts/index.json', {
+        method: 'get'
+    }).then(
+        res => res.json()
+    ).then(
+        res => {
+            // index document
+            idx = lunr(function () {
+                this.ref('url')
+                this.field('title')
+                this.field('content')
+                res.forEach(function (doc) {
+                    this.add(doc)
+                    documents[doc.url] = {
+                        'title': doc.title,
+                        'content': doc.content,
+                    }
+                }, this)
+            })
+            // when data is registered, handle register 
+            registerSearchHandler(searchInput)
+        }
+    ).catch(
+        err => {
+            // if couldn't fetch show error
+            let error = document.createElement('p')
+            error.appendChild(document.createTextNode(`${err}`))
+            searchResults.appendChild(error)
+        }
+    )
+
+    function registerSearchHandler() {
+        // register on input event
+        searchInput.oninput = handleInput
+        // set focus on search input and remove loading placeholder
+        searchInput.focus()
+        searchInput.placeholder = 'Quiero leer sobre...'
+        searchInput.disabled = false
+    }
+
+    function handleInput(event) {
+        // remove search results if the user empties the search input field
+        if (event.target.value.length === 0) {
+            removeAllChildren(searchResults)
+        } else {
+            // get input value
+            let searchTerm = event.target.value
+            // fuzzy search
+            let results = idx.search(encodeURIComponent(searchTerm) + '*')
+            // render results
+            renderSearchResults(results)
+        }
+    }
+
+    function removeAllChildren(el) {
+        while (el.firstChild) {
+            el.removeChild(el.firstChild);
+        }
+    }
+
+    function renderSearchResults(results) {
+
+        if (results.length > 0) {
+            // Limit results to 10
+            if (results.length > 9) {
+                results = results.slice(0, 10)
+            }
+    
+            // empty search Results
+            removeAllChildren(searchResults)
+    
+            results.forEach(result => {
+                // append and article 
+                let article = document.createElement('article')
+                let titleLink = document.createElement('a')
+                let summaryLink = document.createElement('a')
+                let summary = document.createElement('p')
+                let heading = document.createElement('h3')
+    
+                // prepare title link
+                titleLink.href = summaryLink.href = result.ref
+                titleLink.classList.add("search-item-title")
+    
+                // prepare summary link
+                summaryLink.classList.add("search-item-summary")
+                summaryLink.appendChild(document.createTextNode(`${documents[result.ref].content.slice(0, 300)}...`))
+    
+                // create heading content
+                heading.appendChild(document.createTextNode(documents[result.ref].title))
+    
+                // add class to anchor
+                titleLink.appendChild(heading)
+                summary.appendChild(summaryLink)
+    
+                // append article components 
+                article.appendChild(titleLink)
+                article.appendChild(summary)
+    
+                searchResults.appendChild(article)
+            })
+    
+            // empty results
+        } else {
+            removeAllChildren(searchResults)
+            let notFound = document.createElement('p')
+            notFound.appendChild(document.createTextNode('No se encontraron resultados'))
+            searchResults.appendChild(notFound)
+        }
+    }
+}
+
+
+document.addEventListener("turbolinks:load", initializeLunrSearch);
