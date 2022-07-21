8d35f7b84d21a5f5bbebb3ddcedc76a2865316a6
Eduardo Zepeda <eduardozepeda@coffeebytes.dev>
2022-07-20 19:28:00 -0500

    Add a show up modal at bottom of the screen
    - When closed, a cookie is set, modal won't show for 7 days
    - When the successful registration page is shown, a cookie is set, modal
      won't show for 365 days
    - The modal will reveal the subscription form when clicked
    - Minor CSS changes to fit the modal without changing the UI

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,23 @@
+function setCookie(name, value, days) {
+    let expires = ''
+    if (days) {
+        const date = new Date()
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000))
+        expires = '; path=/; expires=' + date.toUTCString()
+    }
+    document.cookie = name + '=' + (value || '') + expires + ';'
+}
+
+function getCookie(name) {
+    const cookies = document.cookie.split(';')
+    for (const cookie of cookies) {
+        if (cookie.indexOf(name + '=') > -1) {
+            return cookie.split('=')[1]
+        }
+    }
+    return null
+}
+
 function addSwitchThemeListener(event) {
     let themeSwitcher = document.getElementById("toggle-label")
     let currentTheme = document.documentElement.getAttribute("data-theme")
@@ -58,10 +78,41 @@ function addScrolltoTopListener(event) {
     scrollUp.onclick = scrollToTop
 }
 
+function addSubscribeFormListener(event) {
+    const subscribeForm = document.getElementById("subscribe-form-modal")
+    const subscribeFormBody = document.getElementById("subscribe-form-body")
+    const close = document.getElementById("subscribe-form-close-icon")
+    const showFormButton = document.getElementById("show-subscribe-form")
+    function closeSubscribeForm() {
+        subscribeForm.classList.add("display-none")
+        setCookie("mail_chimp_subscribe_shown", true, 7)
+    }
+    function toggleSubscribeFormBody() {
+        if (subscribeFormBody.classList.contains("display-none")) {
+            subscribeFormBody.classList.remove("display-none")
+        } else {
+            subscribeFormBody.classList.add("display-none")
+        }
+    }
+    close.onclick = closeSubscribeForm
+    showFormButton.onclick = toggleSubscribeFormBody
+}
+
+function setSubscribeFormTimeout(event) {
+    setTimeout(function () {
+        const subscribeForm = document.getElementById("subscribe-form-modal")
+        subscribeForm.classList.remove("display-none")
+    }, 7500)
+}
+
 function loadAllListeners(event) {
     addSwitchThemeListener(event)
     addSideBarClickListener(event)
     addScrolltoTopListener(event)
+    addSubscribeFormListener(event)
+    if (!getCookie("mail_chimp_subscribe_shown")) {
+        setSubscribeFormTimeout(event)
+    }
 }
 
 
